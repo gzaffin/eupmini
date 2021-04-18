61a168d1613db4e461a1118c77de5ac92f218256
gzaffin <gzaffin@gmail.com>
2021-04-18 21:51:54 +0200

    Sources ported to UTF-8 encoding. Another commit.

--- sintbl.hpp
+++ sintbl.hpp
@@ -1,5 +1,5 @@
 extern int const sintbl[];
-extern int const powtbl[]; // ???????
+extern int const powtbl[]; // ピッチベンド用
 extern int const frequencyTable[]; // FM
 extern int const freqDeltaTable[]; // FM
 extern int const keyscaleTable[]; // FM
